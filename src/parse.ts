b480af226bca18979decd2bb7b93783fe3e27ef6
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-09-13 21:19:56 -0600

    Stylesheet batch

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,10 @@
-import { DocumentClone, MediaRuleClone } from "./cloner.types";
-import { ParseCSSResult } from "./parse.types";
+import {
+  DocumentClone,
+  MediaRuleClone,
+  StyleRuleClone,
+  StyleSheetClone,
+} from "./cloner.types";
+import { ParseCSSResult, RuleBatch, RuleBatchState } from "./parse.types";
 
 const STYLE_RULE_TYPE = 1;
 const MEDIA_RULE_TYPE = 4;
@@ -19,5 +24,105 @@ function parseCSS(doc: DocumentClone): ParseCSSResult {
       }
     }
   }
+
+  parseStyleSheets(doc.styleSheets, globalBaselineWidth);
+
   return { breakpoints: Array.from(breakpoints) };
+}
+
+function parseStyleSheets(
+  styleSheets: StyleSheetClone[],
+  globalBaselineWidth: number
+): void {
+  for (const styleSheet of styleSheets) {
+    parseStyleSheet(styleSheet, globalBaselineWidth);
+  }
+}
+
+function parseStyleSheet(
+  styleSheet: StyleSheetClone,
+  globalBaselineWidth: number
+): void {
+  const baselineWidth = getStylesheetBaselineWidth(
+    styleSheet,
+    globalBaselineWidth
+  );
+  const ruleBatches = batchStyleSheet(styleSheet, baselineWidth);
+}
+
+function getStylesheetBaselineWidth(
+  styleSheet: StyleSheetClone,
+  globalBaselineWidth: number
+): number {
+  const baselineMediaQuery = styleSheet.cssRules.find(
+    (rule) =>
+      rule.type === MEDIA_RULE_TYPE &&
+      (rule as MediaRuleClone).cssRules.length === 0
+  ) as MediaRuleClone;
+
+  return baselineMediaQuery?.minWidth ?? globalBaselineWidth;
+}
+
+function batchStyleSheet(
+  styleSheet: StyleSheetClone,
+  baselineWidth: number
+): RuleBatch[] {
+  let ruleBatchState: RuleBatchState = {
+    currentStyleRuleBatch: null,
+    ruleBatches: [],
+  };
+  for (const rule of styleSheet.cssRules) {
+    if (rule.type === STYLE_RULE_TYPE) {
+      ruleBatchState = {
+        ...ruleBatchState,
+        ...batchStyleRule(
+          rule as StyleRuleClone,
+          ruleBatchState,
+          baselineWidth
+        ),
+      };
+    } else if (rule.type === MEDIA_RULE_TYPE) {
+      ruleBatchState.ruleBatches.push(
+        newMediaRuleBatch(rule as MediaRuleClone)
+      );
+    }
+  }
+  return ruleBatchState.ruleBatches;
+}
+
+function batchStyleRule(
+  styleRule: StyleRuleClone,
+  ruleBatchState: RuleBatchState,
+  baselineWidth: number
+): RuleBatchState {
+  const newRuleBatchState: RuleBatchState = { ...ruleBatchState };
+
+  if (newRuleBatchState.currentStyleRuleBatch === null) {
+    newRuleBatchState.currentStyleRuleBatch = {
+      width: baselineWidth,
+      styleRules: [],
+      isMediaQuery: false,
+    };
+    newRuleBatchState.ruleBatches = [
+      ...newRuleBatchState.ruleBatches,
+      newRuleBatchState.currentStyleRuleBatch,
+    ];
+  }
+  newRuleBatchState.currentStyleRuleBatch = {
+    ...newRuleBatchState.currentStyleRuleBatch,
+    styleRules: [
+      ...newRuleBatchState.currentStyleRuleBatch.styleRules,
+      styleRule,
+    ],
+  };
+
+  return newRuleBatchState;
+}
+
+function newMediaRuleBatch(mediaRule: MediaRuleClone): RuleBatch {
+  return {
+    width: mediaRule.minWidth,
+    styleRules: mediaRule.cssRules,
+    isMediaQuery: true,
+  };
 }
