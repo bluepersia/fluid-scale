bf07722b02fde0b8990ee77b2d064c56f8523a12
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-09-13 21:01:07 -0600

    ParseCSS init

--- src/parse.ts
+++ src/parse.ts
@@ -1,4 +1,23 @@
+import { DocumentClone, MediaRuleClone } from "./cloner.types";
+import { ParseCSSResult } from "./parse.types";
+
 const STYLE_RULE_TYPE = 1;
 const MEDIA_RULE_TYPE = 4;
 
 export { STYLE_RULE_TYPE, MEDIA_RULE_TYPE };
+
+function parseCSS(doc: DocumentClone): ParseCSSResult {
+  const breakpoints = new Set<number>();
+  let globalBaselineWidth = 375;
+
+  for (const styleSheet of doc.styleSheets) {
+    for (const rule of styleSheet.cssRules) {
+      if (rule.type === MEDIA_RULE_TYPE) {
+        const { minWidth, cssRules } = rule as MediaRuleClone;
+        breakpoints.add(minWidth);
+        if (cssRules.length === 0) globalBaselineWidth = minWidth;
+      }
+    }
+  }
+  return { breakpoints: Array.from(breakpoints) };
+}
